6b3f64a911c89ed3e96f85c5981074225b9453b7
kijmaster <kij_82@hotmail.fr>
2012-09-23 11:59:54 +0200

    Enable to get servers grouped by zones (for MenuBarLayout)

--- rest-server/src/managers/ServerManager.php
+++ rest-server/src/managers/ServerManager.php
@@ -16,4 +16,42 @@ class ServerManager
             echo '{ "error": { "text":' . $e->getMessage() . '}}';
         }
     }
+
+    // Return server collection sorted by zones
+    public static function getServersByZones () {
+
+        $sql = "SELECT * FROM server ORDER BY zone, name";
+        try{
+            $db = DBManager::getDb();
+            $stm = $db->query($sql);
+            $servers = $stm->fetchAll(PDO::FETCH_OBJ);
+            $db = null;
+            echo json_encode(ServerManager::splitServersByZones($servers));
+        }catch(PDOException $e){
+            echo '{ "error": { "text":' . $e->getMessage() . '}}';
+        }
+    }
+
+    private static function splitServersByZones ($servers){
+        $zones = array();
+        $curZone = array();
+        $curZoneName = "";
+        foreach($servers as $server){
+            // New Zone to manage
+            if($server->zone != $curZoneName){
+                if(sizeof($curZone) > 0){
+                    $zones[$curZoneName] = $curZone;
+                }
+                $curZoneName = $server->zone;
+                $curZone = array();
+            }
+            // Add current server to current zone
+            $curZone[] = $server;
+        }
+        // Add last zone
+        if(sizeof($curZone) > 0){
+            $zones[$curZoneName] = $curZone;
+        }
+        return $zones;
+    }
 }
--- rest-server/src/routers/ServerRouter.php
+++ rest-server/src/routers/ServerRouter.php
@@ -6,5 +6,8 @@ class ServerRouter
         $app->get('/servers', function(){
             return ServerManager::getServers();
         });
+        $app->get('/servers/zones', function(){
+            return ServerManager::getServersByZones();
+        });
     }
 }
